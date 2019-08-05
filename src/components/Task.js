11aca64ad970fd32ab88c215bba9dcc9a3808048
mjdude <mo@syncspot.net>
2019-08-05 17:24:35 +0800

    initial commit

---
+++ src/components/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function task({
+  task: { id, title, state },
+  onArchiveTask,
+  onPinTask
+}) {
+  return (
+    <div className="list-item">
+      <input type="text" value={title} readOnly={true} />
+    </div>
+  );
+}
